804a33f38376fb77837dbb4d98082ce4b0d47a49
임성현 <shlim0287@gmail.com>
2024-03-22 11:04:35 +0900

    test : feature브랜치 테스트 코드 수정

--- src/test/java/com/estsoft/blogjpa/BlogJpaApplicationTests.java
+++ src/test/java/com/estsoft/blogjpa/BlogJpaApplicationTests.java
@@ -5,5 +5,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BlogJpaApplicationTests {
-
+    //
+    @Test
+    void print(){
+        System.out.println("asasdasd");
+    }
 }
