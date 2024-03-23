c6e43b4534dda07f5ead1b0a4b0f4807e362e4bb
임성현 <shlim0287@gmail.com>
2024-03-23 10:00:00 +0900

    Swagger 설정 추가

--- src/main/java/com/estsoft/blogjpa/config/WebSecurityConfig.java
+++ src/main/java/com/estsoft/blogjpa/config/WebSecurityConfig.java
@@ -17,7 +17,7 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer configure() {      // 스프링 시큐리티 기능 비활성화
         return web -> web.ignoring().requestMatchers(toH2Console())
-                .requestMatchers("/static/**");
+                .requestMatchers("/static/**","/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html");
     }
 
     // 특정 HTTP 요청에 대한 웹 기반 보안 구성
